35267c47ec8d4090479c7eadd8dd3d6cf2c3e2d9
PH7-Jack <pedrolivertwd@gmail.com>
2022-08-26 02:56:12 -0300

    refactor breadcrumb

--- src/views/breadcrumbs.blade.php
+++ src/views/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="flex items-center text-slate-600"
+    class="flex items-center gap-2 text-slate-600"
     x-data="{
         breadcrumbs: @js($breadcrumbs),
 
@@ -10,23 +10,21 @@
         }
     }"
 >
-    <div class="flex items-center gap-2">
-        <a href="{{ $home }}">
-            <!-- Heroicons v2 home/outline -->
-            <svg class="w-5 h-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+    <a href="{{ $home }}">
+        <!-- Heroicons v2 home/outline -->
+        <svg class="w-5 h-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
+    </a>
+
+    <template x-for="trail in breadcrumbs" :key="trail.url + trail.label">
+        <div class="flex items-center gap-2 text-indigo-600 last:text-gray-400">
+            <!-- Heroicons v2 chevron-right/outline -->
+            <svg class="w-4 h-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
             </svg>
-        </a>
 
-        <template x-for="trail in breadcrumbs" :key="trail.url + trail.label">
-            <div class="flex items-center gap-2 text-indigo-600 last:text-gray-400">
-                <!-- Heroicons v2 chevron-right/outline -->
-                <svg class="w-4 h-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
-
-                <a x-text="trail.label" :href="trail.url && trail.url"></a>
-            </div>
-        </template>
-    </div>
+            <a x-text="trail.label" :href="trail.url && trail.url"></a>
+        </div>
+    </template>
 </div>
