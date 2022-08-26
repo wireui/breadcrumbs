2ba5da0b0719e3ba8b67ca4120d3d28ade8d7d46
PH7-Jack <pedrolivertwd@gmail.com>
2022-08-26 01:09:40 -0300

    mock the app stuff

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,15 @@ use WireUi\Breadcrumb\BreadcrumbServiceProvider;
 
 class TestCase extends Testbench\TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        app('session')->put('_token', 'fake-token-for-testing');
+        config()->set('app.key', 'base64:ybwMUpvbFVa73CgNQqRAsBBA6Tx08Bnt1ZHRwzhMbwc=');
+        config()->set('app.debug', true);
+    }
+
     protected function getPackageProviders($app)
     {
         return [
